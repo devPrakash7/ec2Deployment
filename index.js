fd7f681851d6183179fd821d6421722d96a9689e
Kanha Ransingh <prakash@909>
2024-01-07 19:55:43 +0530

    chamge

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const port = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/name' , (req , res) => res.send('My Name is Prakash'))
+app.get('/name' , (req , res) => res.send('My Name is Prakash'));
+
+app.get('/home' , (req , res) => res.send('Welcome to our Home'))
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
