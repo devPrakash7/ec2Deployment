cc6d3a5fdf0b11b0b4b6d66a3599acc8650b360c
Kanha Ransingh <prakash@909>
2024-01-07 19:18:16 +0530

    chamges

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/name' , (req , res) => res.send('My Name is Prakash'))
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
